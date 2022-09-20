afdcfa3153470bc6a9e5be33c866a3afd4ad9213
Denis19850204 <dg04021985@gmail.com>
2022-09-20 21:32:33 +0300

    correct hw

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 import './css/styles.css';
-import API from './api-service'
+import API from './api-service';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -14,32 +14,36 @@ inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 function onInput(e) {
   e.preventDefault();
   const name = e.target.value.trim();
+  
 
   searchCountry.innerHTML = '';
   countryInfo.innerHTML = '';
 
-  API.fetchCountries(name)
-    .then(renderCountries)
-    .catch(error =>
-      Notiflix.Notify.failure('Oops, there is no country with that name')
-    );
+  if (name === '') {
+    return;
+  } else {
+    API.fetchCountries(name)
+      .then(renderCountries)
+      .catch(error =>
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+      );
+  }
 }
 
 function renderCountries(country) {
   if (country.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
-    ); return;
+    );
+    return;
   } else if (country.length > 2 && country.length < 10) {
-    // console.log(
-    //   country.map(({ name, flags }) => `${name}, ${flags.svg}`).join(' ')
-    // );
-    country
-      .map(({ name, flags }) => {
-        const searchTemplate = `<li><img src="${flags.svg}" class="flag" alt="flags" width=20px>${name}</li>`;
-        searchCountry.insertAdjacentHTML('beforeend', searchTemplate);
-      })
-      .join(' '); return;
+    
+    const searchTemplate = country.map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" class="flag" alt="flags" width=20px>${name}</li>`
+    }).join('')
+    
+    searchCountry.insertAdjacentHTML('beforeend', searchTemplate);
+    
   } else if (country.length === 1) {
     country.map(({ flags, name, capital, population, languages }) => {
       const template = `<div class="country-card">
@@ -51,8 +55,10 @@ function renderCountries(country) {
         <li>Languages: <span>${languages.map(lang => lang.name)}</span></li>
       </ul>
     </div>`;
+      
       countryInfo.innerHTML = template;
-    }); return;
+    });
+    return;
   }
 }
 
@@ -64,5 +70,5 @@ function renderCountries(country) {
 //       if (!response.ok) {
 //         throw new Error(response.status);
 //       } return response.json();
-//   })  
+//   })
 // }
